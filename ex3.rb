6d41c31ba735ec84ad001367f8fbe836e040da7f
Matthew Gibbs <mygibbs@gmail.com>
2012-07-01 08:45:39 -0400

    Initial commit

---
+++ ex3.rb
@@ -0,0 +1,29 @@
+puts "I will now count my chickens:"
+
+# divides 30 by 6 first, then adds 25
+puts "Hens", 25 + 30 / 6
+# multiplies 3 by 25, then divides by 4 and gets the remainder
+# then it subtracts that number from 100
+puts "Roosters", 100 - 25 * 3 % 4
+
+puts "Now I will count the eggs:"
+
+# takes the remainder of 4 / 2, subtracts that from the result of
+# 1/4, then adds or subtracts the rest in any order
+puts 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4.0 + 6
+
+puts "Is it true that 3 + 2 < 5 - 7?"
+
+# evaluates this expression as true or false
+puts 3 + 2 < 5 - 7
+
+puts "What is 3 + 2?" , 3 + 2
+puts "What is 5 - 7?", 5 - 7
+
+puts "Oh, that's why it's false."
+
+puts "How about some more."
+
+puts "Is it greater?", 5 > -2
+puts "Is it greater or equal?", 5 >= -2
+puts "Is it less or equal?", 5 <= -2
